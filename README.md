a91addefc6b465175eab1c913ec22d6f7bdb023d
Johannes Reker <info@jreker.de>
2019-05-03 11:53:58 +0200

    Added GraphQL Code

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # GraphQL-Tutorial
 This is a project i have made to learn GraphQL. You can just take it and do what you want.
+This project is based on the tutorial from https://graphql.org
 
 ## Getting started
 
 To get started just install dependencies via npm:
 ```
 npm install
+```
+
+## Example GraphiQL Query
+```
+{
+  hello,
+  quoteOfTheDay,
+  getNumbers(numberCount: 5)
+}
 ```
--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-var { graphql, buildSchema } = require('graphql');
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
 
+
+//Construct schema with schema language
 var schema = buildSchema(`
     type Query {
         hello: String
+        quoteOfTheDay: String!
+        getNumbers(numberCount: Int!): [Int]
     }
 `);
 
+var root = {
+    hello: () => {
+        return 'Hello World!';
+    },
+    quoteOfTheDay: () => {
+        return 'Learn programming not programming languages, everything else is syntax. (Unknown)'
+    },
+    getNumbers: function(args) {
+        var output = [];
+        for(var i = 0;i < args.numberCount;i++) {
+            output.push(Math.round(Math.random() * 10000))
+        }
+        return output;
+    }
+};
+
+var app = express();
+app.use('/graphql',graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(4000);
+console.log("Running GraphQL API Server at localhost:4000/graphql")
